0f9f1883cb4f624aee310899407e73c4c5546362
MahmoudReda <mahreda19@gmail.com>
2026-02-09 21:23:17 +0200

    create models using and start creat forms

--- mySite/app/admin.py
+++ mySite/app/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import  Expense, Income, Category
+
+@admin.register(Income)
+class IncomeAdmin(admin.ModelAdmin):
+    list_display = ('user', 'source', 'amount', 'category', 'description' ,'date_received' )
+    list_filter = ('date_received', 'category')
+    search_fields = ('user','amount','category')
+
+
+@admin.register(Expense)
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = ('user', 'amount', 'category', 'description' , 'date_incurred', 'receipt_image')
+    list_filter = ('date_incurred', 'category')
+    search_fields = ('user','amount','category','description','date_incurred')
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name','type')
+    search_fields = ('name','type')
--- mySite/app/models.py
+++ mySite/app/models.py
@@ -1,3 +1,55 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+CATEGORIES =(
+    ('income', 'Income'),
+    ('expense', 'Expense')
+)
+
+
+class  Category(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    type = models.CharField(max_length=50, choices=CATEGORIES)
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+
+    def __str__(self):
+        return self.name
+
+
+class Income(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount = models.DecimalField('Amount',max_digits=64, decimal_places=2)
+    source = models.CharField(max_length=100)
+    date_received = models.DateField('Date Received')
+
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+    description = models.TextField('Description', null=True , blank=True)
+
+    date_created = models.DateTimeField('Date Created', auto_now_add=True)
+    date_updated = models.DateTimeField('Date Updated', auto_now=True)
+
+    def __str__(self):
+        return f"{self.amount} From {self.source} by {self.user}"
+
+
+class Expense(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount = models.DecimalField('Amount',max_digits=64, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+    date_incurred = models.DateField('Date Incurred')
+    description = models.TextField('Description', null=True , blank=True)
+
+    receipt_image = models.ImageField('Receipt Image', upload_to='receipts/', null=True, blank=True)
+
+    date_created = models.DateTimeField('Date Created', auto_now_add=True)
+    date_updated = models.DateTimeField('Date Updated', auto_now=True)
+
+    def __str__(self):
+        return f"{self.amount} on {self.date_incurred} for {self.category}"
